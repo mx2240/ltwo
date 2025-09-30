f720b1aa23e5383b721cd7c9cfd6fb3383c2af7b
mx2240 <lovemicky10@gmail.com>
2025-09-30 11:24:13 -0700

    up

---
+++ src/componets/SendButton.jsx
@@ -0,0 +1,34 @@
+
+
+
+
+function SendButton() {
+
+
+
+    return (
+
+
+        <>
+
+            <button> send</button>
+
+
+
+
+
+        </>
+
+
+
+
+
+    );
+
+
+
+
+
+}
+
+export default SendButton
